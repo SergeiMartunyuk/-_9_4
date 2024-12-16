676ee5520465c2f896879c25e05f67955f9fa4d3
SergeiMartunyuk <alessaralfinit@gmail.com>
2024-12-16 21:30:04 +0500

    Update Создание функ на лету.py

--- Создание функ на лету.py
+++ Создание функ на лету.py
@@ -5,20 +5,16 @@ second = 'Рамена мало было'
 
 print(list(map(lambda x, y : x == y, first, second)))
 
-
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, mode='w', encoding='UTF-8') as file:
             for i in data_set:
                 file.write(str(i))
                 file.write('\n')
-
     return write_everything
-
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-
 
 class MysticBall:
     def __init__(self, *word):
@@ -27,7 +23,6 @@ class MysticBall:
     def __call__(self):
         word = choice(self.words)
         return word
-
 
 first_ball = MysticBall('Да', 'Нет', 'Наверное')
 print(first_ball())
